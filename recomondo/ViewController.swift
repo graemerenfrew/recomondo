e7ecd975477b9a6020d51d65f533e87c67e854a3
graemerenfrew <graemerenfrew+github@gmail.com>
2018-11-19 23:23:44 +0100

    basic functioning setup screen with icon validation

--- recomondo/ViewController.swift
+++ recomondo/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
-
+    
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         // getting this image into code is a real ball-ache
@@ -26,15 +28,32 @@ class ViewController: UIViewController {
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03) //black with low alpha
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
+        
+        //this is like a listener
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
+    @objc func handleTextInputChange(){
+        let isFormValid = emailTextField.text?.characters.count  ?? 0 > 0 && usernameTextField.text?.characters.count  ?? 0 > 0 && passwordTextField.text?.characters.count  ?? 0 > 0
+        
+        if isFormValid {
+            signUpButton.isEnabled = true
+            signUpButton.backgroundColor = UIColor.rgb(red: 17, green: 154, blue: 237)
+        }
+        else
+        {
+            signUpButton.isEnabled = false
+            signUpButton.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        }
+    }
     let usernameTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Username"
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03) //black with low alpha
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
@@ -45,19 +64,37 @@ class ViewController: UIViewController {
         tf.borderStyle = .roundedRect
         tf.isSecureTextEntry = true
         tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     
     let signUpButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Sign Up", for: .normal)
-        button.backgroundColor = .blue
         button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
         button.layer.cornerRadius = 5
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         button.setTitleColor(.white, for: .normal)
+        
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        
+        button.isEnabled = false //by default
         return button
     }()
+    
+    @objc func handleSignUp(){
+        guard let email = emailTextField.text, email.characters.count > 0 else {return}
+        guard let username = usernameTextField.text, username.characters.count > 0 else {return}
+        guard let password = passwordTextField.text, password.characters.count > 0  else {return}
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            if let err = error {
+                print("Failed", err)
+            }
+            print("Success", authResult?.user.uid ?? "")
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,18 +105,11 @@ class ViewController: UIViewController {
         //now centre in x axis
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         setupInputFields()
-    
+        
     }
     
     fileprivate func setupInputFields() {
         //quick way to create multiple input fields
-        
-        //let greenView = UIView()
-        //greenView.backgroundColor = .green
-        //let redView = UIView()
-        //redView.backgroundColor = .red
-        //let blueView = UIView()
-       //blueView.backgroundColor = .blue
         
         let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
         
@@ -88,11 +118,11 @@ class ViewController: UIViewController {
         stackView.spacing = 10
         
         view.addSubview(stackView)
-    
+        
         stackView.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 20, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 200)
         
     }
-
+    
 }
 
 extension UIView{
@@ -116,12 +146,12 @@ extension UIView{
             self.widthAnchor.constraint(equalToConstant: width).isActive = true
         }
         if height != 0 {
-             self.heightAnchor.constraint(equalToConstant: height).isActive = true
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
         }
         
         
-     
-    
+        
+        
     }
 }
 
