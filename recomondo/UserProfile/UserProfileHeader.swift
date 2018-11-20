6b84cdca62f3bc341a62569deb8e9d8010431fda
graemerenfrew <graemerenfrew+github@gmail.com>
2018-11-20 08:36:00 +0100

    stack views for the post counts and buttons

--- recomondo/UserProfile/UserProfileHeader.swift
+++ recomondo/UserProfile/UserProfileHeader.swift
@@ -11,29 +11,104 @@ import UIKit
 
 class UserProfileHeader: UICollectionViewCell {
     
+    var user: User? {
+        didSet {
+            setupProfileImage()
+            userNameLabel.text = self.user?.username
+        }
+    }
+    
+    
     let profileImageView: UIImageView = {
         let iv = UIImageView()
-        iv.backgroundColor = .red
         return iv
     }()
     
+    //UIStackView to create the header area
+    let gridButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "grid"), for: .normal)
+        return button
+    }()
+    let listButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "list"), for: .normal)
+        button.tintColor = UIColor(white:0, alpha: 0.1)
+        return button
+    }()
+    let bookmarkButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "ribbon"), for: .normal)
+        button.tintColor = UIColor(white:0, alpha: 0.1)
+        return button
+    }()
+    let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "username"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    let postsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "4\nrecommendations"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    let followersLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2\nfollowers"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    let followingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "3\nfollowing"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+
     override init(frame: CGRect){
         super.init(frame: frame)
         
-        backgroundColor = .blue
+        //backgroundColor = .blue
         addSubview(profileImageView)
         profileImageView.anchor(top: topAnchor, left: self.leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 80, height: 80)
         profileImageView.layer.cornerRadius = 80/2
         profileImageView.clipsToBounds = true
         
-        //setupProfileImage()
+        //arrange the 'toolbar'
+        setupBottomToolbar()
         
-    }
+        //arrange the username label
+        setupNameLabel()
+        
+        setupUserStatsView()
     
-    var user: User? {
-        didSet {
-            setupProfileImage()
-        }
+    }
+    fileprivate func setupUserStatsView(){
+        let stackView = UIStackView(arrangedSubviews: [postsLabel, followingLabel, followersLabel])
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        addSubview(stackView)
+        
+        stackView.anchor(top: self.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: rightAnchor, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 12, width: 0, height: 50)
+    }
+    fileprivate func setupNameLabel(){
+        addSubview(userNameLabel)
+        userNameLabel.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, bottom: gridButton.topAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 12, paddingBottom: 0, paddingRight: 12, width: 0, height: 0)
+    }
+    fileprivate func setupBottomToolbar() {
+        let stackView = UIStackView(arrangedSubviews: [gridButton, listButton, bookmarkButton])
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        addSubview(stackView)
+        stackView.anchor(top: nil, left: leftAnchor, bottom: self.bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
     }
     
     fileprivate func setupProfileImage() {
