4383a1dd8b0997237e8f75d96f9fe6d412baf00d
graemerenfrew <graemerenfrew+github@gmail.com>
2018-11-21 12:07:53 +0100

    photo cell created

---
+++ recomondo/AddPost/PhotoSelectorCell.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoSelectorCell.swift
+//  recomondo
+//
+//  Created by Graeme Renfrew on 21/11/2018.
+//  Copyright © 2018 The Polestone Consulting Team. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class PhotoSelectorCell: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        //self.backgroundColor = .brown
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- recomondo/AddPost/PhotoSelectorController.swift
+++ recomondo/AddPost/PhotoSelectorController.swift
@@ -8,11 +8,112 @@
 
 import Foundation
 import UIKit
+import Photos
 
-class PhotoSelectorController: UICollectionViewController {
+class PhotoSelectorController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    let cellId = "cellId"
+    let headerId = "headerId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = .yellow
+        
+        setupNavigationButtons()
+        
+        collectionView?.register(PhotoSelectorCell.self, forCellWithReuseIdentifier: cellId)
+        
+        //put the custom header in the view
+        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
+        
+        print("DEBUG**** About to call fetchPhotos ****")
+        fetchPhotos()
+        
+    }
+    
+    var images = [UIImage]()
+    
+    fileprivate func fetchPhotos()
+    {
+        print("DEBUG****Fetching photos")
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.fetchLimit = 10
+        
+        let allPhotos = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+        allPhotos.enumerateObjects( { (asset, count, stop) in
+
+            let imageManager = PHImageManager.default()
+            let targetSize = CGSize(width: 350, height: 350)
+            let options = PHImageRequestOptions()
+            options.isSynchronous = true
+            imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options, resultHandler: { (image, info) in
+                if let image = image {
+                    self.images.append(image)
+                }
+                //once we have the images, reload the view
+                if count == allPhotos.count - 1 {
+                    self.collectionView?.reloadData()
+                }
+            })
+            
+        })
+    }
+    
+    //build the header
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let width = view.frame.width  //square
+        return CGSize(width: width, height: width)
+    }
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+        
+        header.backgroundColor  = .red
+        return header
+    }
+    
+    //how many elements in this collection view?
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    //what size will the items be?
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 3) / 4
+        return CGSize(width: width, height: width)
+    }
+    //what will the spacing be?
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    //the elements will be cells
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PhotoSelectorCell
+        
+        //cell.backgroundColor = .blue
+        return cell
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    fileprivate func setupNavigationButtons() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(handleNext))
+    }
+    
+    @objc func handleCancel()
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleNext()
+    {
+        print("handling next")
     }
 }
