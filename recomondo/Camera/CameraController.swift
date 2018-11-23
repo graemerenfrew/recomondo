e079f54f6035aa6eea2bb13f84e1f774e266fb0d
graemerenfrew <graemerenfrew+github@gmail.com>
2018-11-23 17:58:04 +0100

    camera transitions working

--- recomondo/Camera/CameraController.swift
+++ recomondo/Camera/CameraController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
+class CameraController: UIViewController, AVCapturePhotoCaptureDelegate, UIViewControllerTransitioningDelegate {
     
     let dismissButton: UIButton = {
         let button = UIButton(type: .system)
@@ -32,9 +32,22 @@ class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //create the transitions
+        transitioningDelegate = self
         setupCaptureSession()
         setupHUD()
         
+    }
+    
+    //transition overrides
+    let customAnimationPresentor = CustomAnimationPresentor()
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return customAnimationPresentor
+    }
+    
+    let customAnimationDismisser = CustomAnimationDismisser()
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return customAnimationDismisser
     }
     
     override var prefersStatusBarHidden: Bool {
